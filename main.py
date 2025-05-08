e8cd66888cae27cd865de0b36135858dc5dd2aa7
Jihagen <j.i.hagen@t-online.de>
2025-05-08 11:28:37 +0200

    main updated

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from pattern import *
 
+def main():
+    checkerboard = Checker(resolution=800, tile_size=50)
+    checkerboard.draw()
+    checkerboard.show()
 
-"""
-checkerboard = Checker(resolution=800, tile_size=50)
-checkerboard.draw()
-checkerboard.show()
-"""
+    circle = Circle(800,200,(200,150))
+    circle.draw()
+    circle.show()
 
-"""
-circle = Circle(800,200,(200,150))
-circle.draw()
-circle.show()
-"""
+    spectrum = Spectrum(resolution=800)
+    spectrum.draw()
+    spectrum.show()
 
-spectrum = Spectrum(resolution=800)
-spectrum_pattern = spectrum.draw()
-spectrum.show()
+if __name__ == "__main__":
+    main()
